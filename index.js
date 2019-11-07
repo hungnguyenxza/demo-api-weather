e9b757318e68b82d8165e39658005c077bab2647
Hung Nguyen Xuan <hungnx@pixelz.com>
2019-11-07 10:10:37 +0700

    init project

---
+++ index.js
@@ -0,0 +1,18 @@
+let express = require("express");
+let body_parser = require("body-parser");
+
+let http = require("http");
+let app = express();
+let myRoute = require('./my-module');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(body_parser.urlencoded({ extended : true}));
+app.use(body_parser.json());
+myRoute(app);
+http.createServer(app).listen(process.env.PORT || 8080, function (){
+  console.log("Node js server is running...");
+});
---
+++ my-module.js
@@ -0,0 +1,8 @@
+let path = require('path');
+
+module.exports = function(app){
+  app.route('/')
+    .get(function(req, res){
+      res.sendFile(path.join(__dirname, 'index.html'));
+    });
+}
